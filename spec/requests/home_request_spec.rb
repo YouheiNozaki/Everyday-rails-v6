e9012334a221f1198d9e1520dbb6b51644c05476
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-02-25 22:29:46 +0900

    roo_oath test

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-
+  describe "#index" do
+    #正常にレスポンスを返すこと
+    it "responds successfully" do
+      get root_path
+      expect(response).to be_successful
+    end
+  end
 end

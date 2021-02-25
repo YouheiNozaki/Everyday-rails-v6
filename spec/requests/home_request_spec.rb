80783e81bd1799be85593c360ce70fe47805d60a
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-02-25 22:32:50 +0900

    root_path test

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe "Homes", type: :request do
   describe "#index" do
     #正常にレスポンスを返すこと
-    it "responds successfully" do
+    it "responds successfully returns a 200 response" do
       get root_path
       expect(response).to be_successful
+      expect(response).to have_http_status "200"
     end
   end
 end

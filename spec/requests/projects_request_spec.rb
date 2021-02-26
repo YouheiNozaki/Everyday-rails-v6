b88c54d0e78a493e1dad331cfaacf4a0c73badfe
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-02-26 11:18:56 +0900

    test create post method

--- spec/requests/projects_request_spec.rb
+++ spec/requests/projects_request_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe "Projects", type: :request do
       #正常にレスポンスを返すこと
       it "responds successfully returns a 200 response" do
         sign_in @user
-        get project_url id: @project.id
+        get project_url @project.id
         expect(response).to be_successful
       end
     end
@@ -55,6 +55,40 @@ RSpec.describe "Projects", type: :request do
         sign_in @user
         get project_url @project.id
         expect(response).to redirect_to root_path
+      end
+    end
+  end
+  describe "#create" do
+    #認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      #プロジェクトを追加できること
+      it "adds a project" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect {
+          post projects_url, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+
+    #ゲストとして
+    context "as a guest" do
+      #302レスポンスを返すこと
+      it "returns a 302 response" do
+        project_params = FactoryBot.attributes_for(:project)
+        post projects_url, params: { project: project_params }
+        expect(response).to have_http_status "302"
+      end
+
+      #サインイン画面にリダイレクトすること
+      it "redirects to the sign-in page" do
+        project_params = FactoryBot.attributes_for(:project)
+        post projects_url, params: { project: project_params }
+        expect(response).to redirect_to "/users/sign_in"
       end
     end
   end
